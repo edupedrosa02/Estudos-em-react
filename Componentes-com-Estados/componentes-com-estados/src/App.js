205d2668451ad10e9fcbc636d3f3044ebe9622d2
edupedrosa02 <dudu.moreira.pedrosa@gmail.com>
2023-04-13 12:37:03 -0300

    green

--- Componentes-com-Estados/componentes-com-estados/src/App.js
+++ Componentes-com-Estados/componentes-com-estados/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
 
-
  class App extends React.Component {
 constructor(){
   super()
@@ -15,23 +14,23 @@ constructor(){
 }
   handleClick(){
     this.setState((estadoAnterior, _props)=>({
-      numeroDeCliques: estadoAnterior.numeroDeCliques+1
+      numeroDeCliques: estadoAnterior.numeroDeCliques +1
     }))
     };
 
-  // handleChange(){
-  //   numeroDeCliques:1  };
+    getButtonColor(num) {
+      return num % 2 === 0 ? 'green' : 'white'
+    };
 
-  // handleChange2(){
-  //   console.log('mudou2');
-  // };
   render() {
-    const {numeroDeCliques} = this.state
+    const {numeroDeCliques, getButtonColor} = this.state;
     return (
       <div>
-        <button type="button" onClick={ this.handleClick }>{numeroDeCliques}</button>
-        {/* <button type="button" onClick={ this.handleChange }>Botão 2</button>
-        <button type="button" onPointerMove={ this.handleChange2 }>Botão 3</button> */}
+        <button type="button"
+         onClick={ this.handleClick }
+         style={{backgroundColor: this.getButtonColor(numeroDeCliques)} }>
+          {`Cliques botão: ${numeroDeCliques}`}
+          </button>
       </div>
     )
   }
