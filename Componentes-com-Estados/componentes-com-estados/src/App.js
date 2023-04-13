80e2ed13f08041d44de7df0494e12e718c233fb7
edupedrosa02 <dudu.moreira.pedrosa@gmail.com>
2023-04-13 12:10:46 -0300

    fim modulo

--- Componentes-com-Estados/componentes-com-estados/src/App.js
+++ Componentes-com-Estados/componentes-com-estados/src/App.js
@@ -5,25 +5,33 @@ import React from 'react';
  class App extends React.Component {
 constructor(){
   super()
+  this.state={
+    numeroDeCliques:0
+  }
+
   this.handleClick=this.handleClick.bind(this);
-  this.handleChange=this.handleClick.bind(this);
-  this.handleChange2=this.handleClick.bind(this);
+  // this.handleChange=this.handleClick.bind(this);
+  // this.handleChange2=this.handleClick.bind(this);
 }
   handleClick(){
-    console.log('Clicou!');
-  };
-  handleChange(){
-    console.log('mudou');
-  };
-  handleChange2(){
-    console.log('mudou2');
-  };
+    this.setState((estadoAnterior, _props)=>({
+      numeroDeCliques: estadoAnterior.numeroDeCliques+1
+    }))
+    };
+
+  // handleChange(){
+  //   numeroDeCliques:1  };
+
+  // handleChange2(){
+  //   console.log('mudou2');
+  // };
   render() {
+    const {numeroDeCliques} = this.state
     return (
       <div>
-        <button type="button" onClick={ this.handleClick }>Botão 1</button>
-        <button type="button" onClick={ this.handleChange }>Botão 2</button>
-        <button type="button" onPointerMove={ this.handleChange2 }>Botão 3</button>
+        <button type="button" onClick={ this.handleClick }>{numeroDeCliques}</button>
+        {/* <button type="button" onClick={ this.handleChange }>Botão 2</button>
+        <button type="button" onPointerMove={ this.handleChange2 }>Botão 3</button> */}
       </div>
     )
   }
